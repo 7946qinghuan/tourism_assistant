6f8fa3a15b8a39ce3f69feda0f672c84de2a5f76
7946qinghuan <1933472218@qq.com>
2026-02-23 22:02:01 +0800

    feat: 增加agent管理

---
+++ tourism_assistant/backend/agent/__init__.py
@@ -0,0 +1,3 @@
+from .agent import AgentEntry, BaseAgentManager, LRUAgentManager, ModelProvider, TTLAgentManager
+
+__all__ = ["AgentEntry", "BaseAgentManager", "LRUAgentManager", "TTLAgentManager", "ModelProvider"]
